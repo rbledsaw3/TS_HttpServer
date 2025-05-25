e9c0c2f4025d566290a3710787567d7a0e9b9c65
Robert Bledsaw III <rbledsaw3@outlook.com>
2025-05-25 13:17:03 -0400

    feat: Add JSON parsing middleware and chirp validation handler
    - Import and use `express.json()` to automatically parse JSON request bodies
    - Wire the POST `/api/validate_chirp` route to the new `handlerChirpsValidate` function
    - Chirp shape is validated along with length of the chirp

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 
 import  { handlerReadiness } from "./api/readiness.js";
+import  { handlerChirpsValidate } from "./api/chirps.js";
 import  { handlerReset } from "./admin/reset.js";
 import  { handlerMetrics } from "./admin/metrics.js";
 import {
@@ -12,9 +13,11 @@ const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponse);
+app.use(express.json());
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
 app.get("/api/healthz", handlerReadiness);
+app.post("/api/validate_chirp", handlerChirpsValidate);
 
 app.get("/admin/metrics", handlerMetrics);
 app.post("/admin/reset", handlerReset);
